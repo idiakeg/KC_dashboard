510bdc9065c86b2155b2f0595e836c3bc3b29c4c
idiakeg <idiakeg@gmail.com>
2024-06-22 16:15:33 +0100

    dashboard sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Customers from "./pages/Customers";
 
 function App() {
     return (
@@ -10,6 +12,9 @@ function App() {
                 <Route index path="/" element={<Home />} />
                 <Route index path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />}>
+                    <Route index element={<Customers />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
